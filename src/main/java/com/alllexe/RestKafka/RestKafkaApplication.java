b60570c13ab2d0138a4210c94c24b9111db17459
alabra <robobob8>
2023-12-20 20:45:31 +0300

    init commit

---
+++ src/main/java/com/alllexe/RestKafka/RestKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.alllexe.RestKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestKafkaApplication.class, args);
+	}
+
+}
